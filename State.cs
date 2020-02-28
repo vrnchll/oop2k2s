c4bc3f948a6697612bc02d1fd834b04726470ba2
vrnchll <54811120+vrnchll@users.noreply.github.com>
2020-02-28 21:50:08 +0200

    Лабораторная №3.Паттерн State

---
+++ State.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace patterns_3
+{
+    
+    abstract class State
+        {
+            public abstract void HandleInput(MyObject redridhood);
+        };
+    class Stand : State
+    {
+        public override void HandleInput(MyObject redridhood)
+        {
+            redridhood.State = new Fire();
+            Console.WriteLine("Стоит!");
+        }
+    }
+    class Jump : State
+    {
+        public override void HandleInput(MyObject redridhood)
+        {
+            redridhood.State = new Stand();
+            Console.WriteLine("Огоонь!");
+
+        }
+        
+    }
+   
+    class Fire : State
+    {
+        public override void HandleInput(MyObject redridhood)
+        {
+            redridhood.State = new Jump();
+            Console.WriteLine("Прыжоок!");
+        }
+    }
+    class MyObject
+        {
+        public State State { get; set; }
+        public MyObject(State state)
+        {
+            this.State = state;
+        }
+        public void Request()
+        {
+            this.State.HandleInput(this);
+        }
+    };
+    }
+
