ab618da21792a39d04267fd42ea69a10934f8469
vrnchll <54811120+vrnchll@users.noreply.github.com>
2020-03-27 11:40:23 +0200

    !!Исправленная!!  Лабораторная работа №6 "RegEx"

---
+++ RegEx_6/Company.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace lab5
+{
+    public partial class Company : Form
+    {
+        public List<Job> Jobs = new List<Job>();
+        public Company()
+        {
+            InitializeComponent();
+        }
+        [Serializable]
+        [XmlRoot(Namespace = "lab5")]
+        [XmlType("Job")]
+        public class Job
+        {
+            [XmlElement(ElementName = "Title")]
+            public string Title { get; set; }
+            [XmlElement(ElementName = "Company")]
+            public string Company { get; set; }
+            [XmlElement(ElementName = "Salary")]
+            public int Salary { get; set; }
+            [XmlElement(ElementName = "Experience")]
+            public int Experience { get; set; }
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            Jobs.Add(new Job {Company = CompanyName1.Text});
+        }
+    }
+}
---
+++ RegEx_6/XmlSerializerWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace lab5
+{
+    public static class XmlSerializerWrapper
+    {
+        public static void Serialize<T>(T obj, string path)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(T));
+            string NewPath = $"{path}{DateTime.Now.Year}{DateTime.Now.Month}{DateTime.Now.Day}_{DateTime.Now.Hour}{DateTime.Now.Minute}{DateTime.Now.Second}.xml";
+
+            using (FileStream fs = new FileStream(NewPath, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, obj);
+            }
+        }
+
+        public static T Deserialize<T>(string path)
+        {
+            T obj;
+            using(FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                XmlSerializer formatter = new XmlSerializer(typeof(T));
+                obj = (T)formatter.Deserialize(fs);
+            }
+
+            return obj;
+        }
+    }
+}
